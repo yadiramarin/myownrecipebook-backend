e3fe6c3561b525bb1f60ca8d7f5bcf73ddbf4f6d
yadiramarin <yadira.marin.gonzalez@gmail.com>
2022-03-23 19:16:32 -0400

    Initial JHipster configuration

---
+++ src/main/java/com/myownrecipebook/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.myownrecipebook.repository;
+
+import com.myownrecipebook.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/myownrecipebook/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.myownrecipebook.web.rest;
